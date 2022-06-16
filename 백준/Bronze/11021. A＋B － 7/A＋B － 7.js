e48e499acd80189d205b1f53c885fc9d76ad9f62
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-06-16 09:03:15 +0900

    [Bronze III] Title: A+B - 7, Time: 136 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+let a =Number(input[0])
+let b, c
+for(i = 1 ; i <= a ; i++){
+     b = input[i].split(' ')
+     c = Number(b[0]) + Number(b[1])
+    console.log(`Case #${i}: ${c}`)
+}
+    

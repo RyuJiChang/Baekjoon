80b307d96ec5edebcb12366f4a1f51c11a88b535
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-08-10 23:28:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 232 ms, Memory: 12908 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+for(let i = 0 ; i < input.length ; i++){
+    input[i] = input[i].split(' ').map(Number)
+}
+
+let dp = []
+let nums = input[0][0]
+let weight = input[0][1]
+for(let i = 0 ; i <= weight ; i++){
+    dp[i] = 0
+}
+
+for(let i = 1 ; i <= nums ; i++){
+    for(let j = weight ; j >= 1 ; j--){
+        if(j >= input[i][0]){
+            dp[j] = Math.max(dp[j] , dp[j-input[i][0]] + input[i][1])
+    }
+}
+}
+
+console.log(dp[weight])
+    

2ea8cd539856cac8995adae59f1767e84d30d375
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-08-18 21:49:45 +0900

    [Silver III] Title: 악수, Time: 548 ms, Memory: 255680 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.js
@@ -0,0 +1,8 @@
+const input = Number(require('fs').readFileSync('/dev/stdin').toString().trim())
+let dp = [1,1]
+
+for(let i = 2 ; i <= input ; i++){
+    dp[i] = (dp[i-1] + dp[i-2])%10
+}
+
+console.log(dp[input])

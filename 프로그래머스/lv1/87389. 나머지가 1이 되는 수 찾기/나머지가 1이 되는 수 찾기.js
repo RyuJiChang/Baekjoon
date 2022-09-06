3d7057edc7a684e44f9eae935245b20661d0bf33
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-09-06 23:59:33 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 2 ; i < n ; i++){
+        if(n%i === 1){
+            answer = i
+            break
+        }
+    }
+    return answer;
+}

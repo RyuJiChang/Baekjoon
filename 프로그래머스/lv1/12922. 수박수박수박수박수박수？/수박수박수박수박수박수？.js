aeb9d1c691f6b3fd06032f514999f1fa61a9022c
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-09-08 00:03:54 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = '';
+    for(let i = 1 ; i <= n ; i++){
+        if(i%2){
+            answer += '수'
+        }
+        else{
+            answer += '박'
+        }
+    }
+    return answer;
+}

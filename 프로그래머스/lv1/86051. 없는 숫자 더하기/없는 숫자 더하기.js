458d8037cb0574f58423944f30499f757039b867
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-09-08 00:14:19 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = 0
+    for(let i = 1 ; i < 10 ; i++){
+        if(numbers.indexOf(i) === -1){
+            answer += i
+        }
+    }
+    return answer;
+}

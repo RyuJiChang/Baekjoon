b75458d365697e60974dacfe2bf04df544277fb0
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-09-10 20:13:44 +0900

    [Silver I] Title: 퇴사 2, Time: 1652 ms, Memory: 170124 KB -BaekjoonHub

---
+++ 백준/Silver/15486. 퇴사 2/퇴사 2.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let num = Number(input[0])
+
+let dp = new Array(num+50).fill(0)
+
+
+for(let i = num ; i > 0 ; i--){
+    let [a,b] = input[i].split(' ').map(Number)
+
+    if(i+a-1 <= num){
+        dp[i] = Math.max(dp[i+a] + b , dp[i+1])
+    }
+    else{
+        dp[i] = dp[i+1]
+    }
+}
+console.log(dp[1])

0efcacedff4425229a8aa0d0acad8cbe959d1d12
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-10-03 23:48:50 +0900

    [unrated] Title: 합성수 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let dp = new Array(n+1).fill(1)
+    for(let i = 2 ; i <= n ; i++){
+        if(dp[i]){
+            for(let j = 2 ; i*j <= n ; j++){
+                dp[i*j] = 0
+            }
+        }
+    }
+    
+    return dp.filter(el => el === 0).length
+}

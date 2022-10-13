2b9aea1f234904f55fcb634a36ffeb6afd7e8e38
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-10-14 08:35:39 +0900

    [Bronze III] Title: Triangles, Time: 136 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
+
+let result = []
+for(let i = 0 ; i < input.length-1 ; i++){
+    for(let j = 1 ; j <= input[i] ; j++){
+        result.push('*'.repeat(j))
+    }
+}
+
+console.log(result.join('\n'))

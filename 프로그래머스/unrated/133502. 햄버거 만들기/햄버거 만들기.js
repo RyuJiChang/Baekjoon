e52403fca7bb697307e5cae82f3e770f8cf27b91
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-11-26 20:55:03 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -13,7 +13,19 @@ function solution(ingredient) {
         }
     }
     return count
-    
+//     function solution(ingredient) {
+//     let count = 0;
+
+//     for (let i = 0; i < ingredient.length; i++) {
+//         if (ingredient.slice(i, i + 4).join('') === '1231') {
+//             count++;
+//             ingredient.splice(i, 4);
+//             i -= 3;
+//         }
+//     }
+
+//     return count;
+
     
     
     // let str = ingredient.join('')

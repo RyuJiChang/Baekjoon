ea9f0f8fc3eb1ee7b9efac20e1da1956ac921292
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-11-26 09:27:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -0,0 +1,3 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('dz=').join('a').split('lj').join('a').split('nj').join('a').split('-').join('').split('=').join('')
+
+console.log(input.length)

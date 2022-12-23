6bfdfb8f5cedf2ec5cc1cde9dc0caaf3a48769e4
RyuJiChang <104156381+RyuJiChang@users.noreply.github.com>
2022-12-23 17:08:48 +0900

    [Bronze III] Title: 사과, Time: 140 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let result = 0
+for(let i = 1 ; i < input.length ; i++){
+    let [a,b] = input[i].split(' ').map(Number)
+    result += b%a
+}
+
+console.log(result)

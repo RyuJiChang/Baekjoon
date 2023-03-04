b8fd2fdca34e8fd544de61c97234c91d4a10967f
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-03-05 08:39:16 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.js
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.js
@@ -1,5 +1,3 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ").map(BigInt)
-let a =input[0]
-let b =input[1]
-let result = '' + a/b + '\n' + a%b;
-console.log(result)
+const [a,b] = require('fs').readFileSync('/dev/stdin').toString().trim().split(" ").map(BigInt)
+console.log(String(a/b))
+console.log(String(a%b))

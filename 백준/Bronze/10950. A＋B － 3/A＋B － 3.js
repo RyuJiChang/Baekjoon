3036651b90a1abdbed82d7b2464ddc2e66928ae7
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-03-08 06:47:08 +0900

    [Bronze V] Title: A+B - 3, Time: 120 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const result = []
+for(i = 1 ; i < input.length ; i++){
+    const [a, b] = input[i].split(' ').map(Number)
+    result.push(a+b)
+}
+console.log(result.join('\n'))

c37b3049941d93cf8742ee29fb9c2316c1ca18ec
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-03-08 08:23:34 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -1,8 +1,8 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
-let b, c
-for(i = 0 ; i < input.length-1 ; i++){
-     b = input[i].split(' ')
-     c = Number(b[0]) + Number(b[1])
-    console.log(c)
+const result = []
+for(let i = 0 ; i < input.length-1 ; i++){
+    const [a,b] = input[i].split(' ').map(Number)
+    result.push(a+b)
 }
-    
+
+console.log(result.join('\n'))

e39e3148ebaf697e0c9658d3ec1e389eb16fae11
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-03-08 16:21:30 +0900

    [Bronze IV] Title: 모음의 개수, Time: 116 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.js
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.js
@@ -1,11 +1,16 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
-const moum = ['a', 'e', 'i', 'o', 'u' , 'A', 'E', 'I', 'O', 'U']
+
+const vowel = {a : 1, e : 1, i : 1, o : 1, u : 1}
+const result = []
+
 for(let i = 0 ; i < input.length - 1 ; i++){
-    let num = 0;
+    let count = 0;
     for(let j = 0 ; j < input[i].length ; j++){
-        if(moum.includes(input[i][j])){
-            num += 1
+        if(vowel[input[i][j].toLowerCase()]){
+            count += 1
         }
     }
-    console.log(num)
+    result.push(count)
 }
+
+console.log(result.join('\n'))

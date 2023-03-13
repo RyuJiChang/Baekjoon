4a72f8ba31ef35e4cd5190f5579b4b4dc33c2e65
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-03-13 16:18:41 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 120 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.js
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.js
@@ -1,15 +1,6 @@
 const input = Number(require('fs').readFileSync('/dev/stdin').toString().trim())
-let result = ''
-for(let i = 0 ; i < input ; i++){
-    for(let j = 1 ; j < input-i ; j++){
-        result += ' '
-    }
-    for(let k = 0 ; k < i+1 ; k++){
-        result += `*`
-        if(k != 0){
-            result += '*'
-        }
-    }
-    console.log(result)
-    result = ''
+const result = []
+for(let i = 1 ; i <= input ; i++){
+    result.push(`${' '.repeat(input-i)}${'*'.repeat(i*2 -1)}`)
 }
+console.log(result.join('\n'))

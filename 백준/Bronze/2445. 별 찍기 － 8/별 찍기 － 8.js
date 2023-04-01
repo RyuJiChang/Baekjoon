647c909fcfa8302e5eaa8d865614751327895293
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-04-01 18:23:55 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 120 ms, Memory: 9536 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
@@ -1,25 +1,11 @@
 const input = Number(require('fs').readFileSync('/dev/stdin').toString().trim())
-let result = ''
-function star(num) {
-    let result = ''
-    for(let i = 0 ; i < num ; i++){
-        result += '*'
-    }
-    return result
-}
+const result = []
 
-function blank(num) {
-    let result = ''
-    for(let i = 0 ; i < num ; i++){
-        result += `  `
-    }
-    return result    
-}
 for(let i = 1 ; i <= input ; i++){
-    result +=`${star(i)}${blank(input-i)}${star(i)}\n`
+    result.push(`${'*'.repeat(i)}${'  '.repeat(input-i)}${'*'.repeat(i)}`)
 }
 for(let i = input-1 ; i >= 1 ; i--){
-    result +=`${star(i)}${blank(input-i)}${star(i)}\n`
+    result.push(`${'*'.repeat(i)}${'  '.repeat(input-i)}${'*'.repeat(i)}`)
 }
 
-console.log(result)
+console.log(result.join('\n'))

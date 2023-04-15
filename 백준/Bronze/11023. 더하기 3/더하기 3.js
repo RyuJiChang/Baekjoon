3f52ac23f87a675fb7afbbf0125e84d8646a5ba8
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-04-15 23:22:20 +0900

    [Bronze III] Title: 더하기 3, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number)
+
+let sum = 0
+
+for(let i = 0 ; i < input.length ; i++){
+    sum += input[i]
+}
+
+console.log(sum)

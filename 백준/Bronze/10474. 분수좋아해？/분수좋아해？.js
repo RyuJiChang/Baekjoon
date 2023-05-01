6c739e0233b7fd6bab480cbb6b3541cfe8e16a25
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-05-02 07:35:48 +0900

    [Bronze III] Title: 분수좋아해?, Time: 136 ms, Memory: 10512 KB -BaekjoonHub

---
+++ 백준/Bronze/10474. 분수좋아해？/분수좋아해？.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const result = []
+
+for(let i = 0 ; i < input.length -1 ; i++){
+    const[a,b] = input[i].split(' ').map(Number)
+    result.push(`${Math.floor(a/b)} ${a%b} / ${b}`)
+}
+
+console.log(result.join('\n'))

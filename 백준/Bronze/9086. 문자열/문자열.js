e4d4ae40869567542c7076089a301922d020ae54
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-08-24 19:38:30 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const result = []
+for(let i = 1 ; i < input.length ; i++){
+    result.push(input[i][0] + input[i][input[i].length-1])
+}
+
+console.log(result.join('\n'))

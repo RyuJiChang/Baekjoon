b895d20e2f99143e378862e97d148c8092e406b4
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2023-10-29 20:24:20 +0900

    [Bronze III] Title: Divvying Up, Time: 188 ms, Memory: 9696 KB -BaekjoonHub

---
+++ 백준/Bronze/20332. Divvying Up/Divvying Up.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')[1].split(' ').map(Number)
+let sum = 0
+
+for(let i = 0 ; i < input.length ; i++){
+    sum += input[i]
+}
+
+console.log(sum % 3 === 0 ? 'yes' : 'no')

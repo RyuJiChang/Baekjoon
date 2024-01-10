728c16a963e031e2512c2a9fdf646db01ae01acb
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-01-10 23:46:21 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 124 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
+let sum = 0
+
+for(let i = 0 ; i < input.length ; i++){
+    sum += input[i]
+}
+
+console.log(Math.floor(sum/60))
+console.log(sum % 60)

6034225c94bdbd767184c888d09de38cd1d2aa0a
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-02-24 23:53:33 +0900

    [Bronze III] Title: Начало, Time: 148 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/29319. Начало/Начало.js
@@ -0,0 +1,7 @@
+const input= require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')[1].split(' ').map(Number)
+let sum = 0
+for(let i = 0 ; i < input.length ; i++){
+    sum += input[i]
+}
+
+console.log(sum - Math.max(...input))

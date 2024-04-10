b1ff8d77bef435b3872ae44d77eba1c1a25cf053
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-04-10 23:52:02 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.js
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.js
@@ -1,4 +1,4 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf-8').trim()[0];
+const input = require('fs').readFileSync('/dev/stdin', 'utf-8')[0];
 if(input === '0'){
     console.log('divide by zero')
 }

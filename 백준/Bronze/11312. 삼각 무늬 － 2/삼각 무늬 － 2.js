87433d243ebe04939702247a3a90cb82386778dd
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-04-17 21:10:46 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 120 ms, Memory: 10020 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const result = []
+
+for(let i = 1 ; i < input.length ; i++){
+    const [a, b] = input[i].split(' ').map(Number)
+    result.push(Math.ceil(a/b)**2)
+}
+
+console.log(result.join('\n'))

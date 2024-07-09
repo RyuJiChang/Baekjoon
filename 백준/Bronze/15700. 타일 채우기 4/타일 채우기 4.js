7dba4c6fff43de545c577fd13188eae0f8945c23
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-07-09 22:15:16 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 108 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.js
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.js
@@ -1,12 +1,2 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(BigInt)
+console.log(String(input[0] * input[1] / 2n))

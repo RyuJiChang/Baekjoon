c3c972f1e9e060d85625da78396f7dec70f8e0f8
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-09-22 23:13:40 +0900

    [Gold IV] Title: 거짓말, Time: 96 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.js
@@ -0,0 +1,52 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(el => el.split(' ').map(Number))
+const [n, m] = input[0]
+const know = input[1].slice(1)
+const parent = []
+let count = 0
+
+for(let i = 0 ; i <= n ; i++){
+    parent[i] = i
+}
+
+const find = (n) => {
+    if (parent[n] != n) {
+        parent[n] = find(parent[n])
+    }
+    return parent[n]
+}
+
+const union =  (a, b) => {
+    const rootA = find(a)
+    const rootB = find(b)
+    
+    if(rootA != rootB){
+        parent[rootB] = rootA
+    }
+}
+
+for(let i = 0 ; i < know.length - 1 ; i++){
+    union(know[i], know[i + 1])
+}
+
+for(let i = 2 ; i < input.length ; i++){
+    for(let j = 1; j < input[i].length - 1; j++){
+        union(input[i][j], input[i][j + 1])
+    }
+}
+
+for(let i = 2 ; i < input.length ; i++){
+    let check = true
+    
+    for(let j = 1 ; j < input[i].length ; j++){
+        if(know.length > 0 && find(input[i][j]) == find(know[0])){
+            check = false
+            break
+        }
+    }
+    
+    if(check){
+        count++
+    }
+}
+
+console.log(count)

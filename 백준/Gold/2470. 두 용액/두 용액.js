000aec991918f14f2234646af44bb451e340fa9d
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2024-11-08 22:27:49 +0900

    [Gold V] Title: 두 용액, Time: 208 ms, Memory: 28328 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')[1].split(' ').map(Number).sort((a,b) => a-b)
+let left = 0
+let right = input.length - 1
+let min = Infinity
+let result = ''
+
+while(left != right){
+    let sum = input[left] + input[right]
+    if(Math.abs(sum) < min){
+        min = Math.min(Math.abs(sum), min)
+        result = `${input[left]} ${input[right]}`
+    }
+    if(sum >= 0){
+        right--
+    }
+    else{
+        left++
+    }
+}
+
+console.log(result)

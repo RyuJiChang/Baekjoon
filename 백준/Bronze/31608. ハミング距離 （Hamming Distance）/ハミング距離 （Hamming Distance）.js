2b01719b2b337d73e6af1e803561cb0713d18a38
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2025-09-01 23:54:44 +0900

    [Bronze IV] Title: ハミング距離 (Hamming Distance), Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/31608. ハミング距離 （Hamming Distance）/ハミング距離 （Hamming Distance）.js
@@ -0,0 +1,11 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+let count = 0
+
+for(let i = 0 ; i < input[1].length ; i++){
+    if(input[1][i] != input[2][i]){
+        count++
+    }
+}
+
+console.log(count)

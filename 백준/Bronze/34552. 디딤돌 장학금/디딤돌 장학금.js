3d87207b2720b535d9b9cf07c94eb0bda1287f2a
류지창 <104156381+RyuJiChang@users.noreply.github.com>
2025-10-14 03:43:27 +0900

    [Bronze IV] Title: 디딤돌 장학금, Time: 92 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Bronze/34552. 디딤돌 장학금/디딤돌 장학금.js
+++ 백준/Bronze/34552. 디딤돌 장학금/디딤돌 장학금.js
@@ -1,14 +1,10 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(el => el.split(' ').map(Number))
-
-const M = input[0]; // 분위별 장학금
-const N = input[1][0]; // 학생 수
-let result = 0;
-
-for (let i = 2; i < 2 + N; i++) {
-  const [B, L, S] = input[i];
-  if (L >= 2.0 && S >= 17) {
-    result += M[B];
-  }
+let result = 0
+    
+for(let i = 2 ; i < input.length ; i++){
+    if(input[i][1] >= 2 && input[i][2] >= 17){
+        result += input[0][input[i][0]]
+    }
 }
 
-console.log(result);
+console.log(result)
